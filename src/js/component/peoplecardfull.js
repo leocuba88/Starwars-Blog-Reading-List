2cd73210428606b1663df1deb7cfead458e8a740
leocuba88 <leocuba88@hotmail.com>
2021-08-06 01:13:26 +0000

    doing

--- src/js/component/peoplecardfull.js
+++ src/js/component/peoplecardfull.js
@@ -1,20 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { PeopleCard } from "../component/peoplecard";
 
 export function PeopleCardFull(props) {
-return <div></div>
+	return (
+		<div>
+			<li>{props.name}</li>
+			<li>{props.height}</li>
+			<li>{props.gender}</li>
+		</div>
+	);
 }
-
 
 PeopleCardFull.propTypes = {
 	name: PropTypes.string,
-    uid: PropTypes.string,
-    height: PropTypes.string,
-    mass: PropTypes.string,
-    hair_color: PropTypes.string,
-      skin_color: PropTypes.string,
-      eye_color: PropTypes.string,
-      birth_year: PropTypes.number,
-      gender: PropTypes.string,
-      description: PropTypes.string,
+	uid: PropTypes.string,
+	height: PropTypes.string,
+	mass: PropTypes.string,
+	hair_color: PropTypes.string,
+	skin_color: PropTypes.string,
+	eye_color: PropTypes.string,
+	birth_year: PropTypes.number,
+	gender: PropTypes.string,
+	description: PropTypes.string
 };
--- src/js/views/PeoplePage.js
+++ src/js/views/PeoplePage.js
@@ -1,18 +1,17 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Link, useParams } from "react-router-dom";
-
-import { Context } from "../store/appContext";
+import { PeopleCardFull } from "../component/peoplecardfull";
 
 export function PeoplePage() {
-	const [peoplePage, setPeoplePage] = useState(null);
+	const [peoplePage, setPeoplePage] = React.useState(null);
 	const params = useParams();
 
 	React.useEffect(() => {
 		fetch("https://www.swapi.tech/api/people/" + params.id)
 			.then(res => res.json())
-			.then(data => setPeoplePage(data.result))
+			.then(data => setPeoplePage(data.result.properties))
 			.catch(err => console.error(err));
 	}, []);
 
-	return <div>{peoplePage !== null && <div>{peoplePage.properties.height}</div>}</div>;
+	return <PeopleCardFull {...(peoplePage !== null ? peoplePage : {})} />;
 }
