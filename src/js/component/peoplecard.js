40e4386ed6b1b986f4cd62f9c5f76a7be11df351
leocuba88 <leocuba88@hotmail.com>
2021-08-07 15:28:02 +0000

    doing

--- src/js/component/peoplecard.js
+++ src/js/component/peoplecard.js
@@ -1,8 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { FavoritesContext } from "../FavoritesContext";
 
 export function PeopleCard(props) {
 	const [peoplePage, setPeoplePage] = React.useState(null);
+	const [favorites, setFavorites] = React.useContext(FavoritesContext);
 
 	React.useEffect(() => {
 		fetch("https://www.swapi.tech/api/people/" + props.uid)
@@ -10,6 +12,16 @@ export function PeopleCard(props) {
 			.then(data => setPeoplePage(data.result))
 			.catch(err => console.error(err));
 	}, []);
+
+	const handleClickFavorite = () => {
+		let newFavorites = favorites.concat([props.name]);
+		setFavorites(newFavorites);
+	};
+
+	const handleClickUnFavorite = () => {
+		let newFavorites = favorites.filter(item => item !== props.name);
+		setFavorites(newFavorites);
+	};
 
 	return (
 		<div className="card-group">
@@ -21,6 +33,15 @@ export function PeopleCard(props) {
 						<a href={"/people/" + props.uid}>Learn more</a>
 						{peoplePage !== null && <div>{peoplePage.properties.height}</div>}
 					</p>
+					{!favorites.includes(props.name) ? (
+						<button className="btn btn-outline-warning" onClick={handleClickFavorite}>
+							❤️
+						</button>
+					) : (
+						<button className="btn btn-warning" onClick={handleClickUnFavorite}>
+							❤️
+						</button>
+					)}
 					<p className="card-text">
 						<small className="text-muted">Last updated 3 mins ago</small>
 					</p>
--- src/js/layout.js
+++ src/js/layout.js
@@ -13,45 +13,49 @@ import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+import { FavoritesContext } from "./FavoritesContext";
 
 //create your first component
 const Layout = () => {
 	//the basename is used when your project is published in a subdirectory and not in the root of the domain
 	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
 	const basename = process.env.BASENAME || "";
+	const favoritesStates = React.useState([]);
 
 	return (
 		<div className="d-flex flex-column">
-			<BrowserRouter basename={basename}>
-				<ScrollToTop>
-					<Switch>
-						<Route exact path="/">
-							<Home />
-						</Route>
-						<Route exact path="/people">
-							<PeopleList />
-						</Route>
-						<Route exact path="/people/:id">
-							<PeoplePage />
-						</Route>
-						<Route exact path="/vehicles/">
-							<VehiclesList />
-						</Route>
-						<Route exact path="/vehicles/:id">
-							<VehiclesPage />
-						</Route>
-						<Route exact path="/planets/">
-							<PlanetsList />
-						</Route>
-						<Route exact path="/planets/:id">
-							<PlanetsPage />
-						</Route>
-						<Route>
-							<h1>Not found!</h1>
-						</Route>
-					</Switch>
-				</ScrollToTop>
-			</BrowserRouter>
+			<FavoritesContext.Provider value={favoritesStates}>
+				<BrowserRouter basename={basename}>
+					<ScrollToTop>
+						<Switch>
+							<Route exact path="/">
+								<Home />
+							</Route>
+							<Route exact path="/people">
+								<PeopleList />
+							</Route>
+							<Route exact path="/people/:id">
+								<PeoplePage />
+							</Route>
+							<Route exact path="/vehicles/">
+								<VehiclesList />
+							</Route>
+							<Route exact path="/vehicles/:id">
+								<VehiclesPage />
+							</Route>
+							<Route exact path="/planets/">
+								<PlanetsList />
+							</Route>
+							<Route exact path="/planets/:id">
+								<PlanetsPage />
+							</Route>
+							<Route>
+								<h1>Not found!</h1>
+							</Route>
+						</Switch>
+					</ScrollToTop>
+				</BrowserRouter>
+			</FavoritesContext.Provider>
 		</div>
 	);
 };
