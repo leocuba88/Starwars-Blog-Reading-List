78ee2e130665fdb0e38fc76906f2eef7e568104e
leocuba88 <leocuba88@hotmail.com>
2021-08-10 21:03:12 +0000

    doing

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
+	const [showMenu, setShowMenu] = React.useState("");
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
@@ -9,6 +10,13 @@ export const Navbar = () => {
 			</Link>
 			<div className="dropdown">
 				<button
+					onClick={() => {
+						if (showMenu == "") {
+							setShowMenu("show");
+						} else if (showMenu == "show") {
+							setShowMenu("");
+						}
+					}}
 					className="btn btn-secondary dropdown-toggle"
 					type="button"
 					id="dropdownMenuButton"
@@ -17,7 +25,7 @@ export const Navbar = () => {
 					aria-expanded="false">
 					Dropdown button
 				</button>
-				<div className="dropdown-menu show" aria-labelledby="dropdownMenuButton">
+				<div className={"dropdown-menu " + showMenu} aria-labelledby="dropdownMenuButton">
 					<a className="dropdown-item" href="#">
 						Action
 					</a>
