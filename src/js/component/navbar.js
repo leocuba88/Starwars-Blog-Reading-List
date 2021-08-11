00fa607205bcec9f55a42198d80117f8d552d3ff
leocuba88 <leocuba88@hotmail.com>
2021-08-11 02:15:35 +0000

    doing

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -13,6 +13,7 @@ export const Navbar = () => {
 					src="https://compass-ssl.xboxlive.com/assets/67/1c/671c739e-386a-4df3-9774-30e9e46eb53a.jpg?n=MSXC-StarWarsTitle-HeroAndImageCard-large-l-794x447-16x9-01.jpg"
 				/>
 			</Link>
+
 			<div className="dropdown">
 				<button
 					onClick={() => {
@@ -22,24 +23,23 @@ export const Navbar = () => {
 							setShowMenu("");
 						}
 					}}
-					className="dropdown_button"
+					className="btn btn-primary"
 					type="button"
 					id="dropdownMenuButton"
 					data-toggle="dropdown"
 					aria-haspopup="true"
 					aria-expanded="false">
 					Favorites
-					<i className="fas fa-sort-down fa-lg downbutton" />
+					<i className="fas fa-star" />
 				</button>
-				<div className={"dropdown-menu " + showMenu} aria-labelledby="dropdownMenuButton">
-					<ul className="dropdown_button" aria-labelledby="dropdownMenuButton1">
+				<div className={"dropdown-menu dropdown-menu-right " + showMenu} ariaLabeledBy="dropdownMenuButton">
+					<ul className="favorite-menu" aria-labelledby="dropdownMenuButton1">
 						{favorites.map((item, index) => {
 							return (
 								<li key={index} className="dropdown-item">
 									<a className="dropdown-item" href="#">
 										{item}
 									</a>
-									<i className="far fa-trash-alt" />
 								</li>
 							);
 						})}
